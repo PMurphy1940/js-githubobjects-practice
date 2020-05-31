e2547b54c418af6a3a7a914458f0b3c5d91af524
Patrick Murphy <engine_ear@mac.com>
2020-05-31 16:32:47 -0500

    added a more complete output with a 'for' loop

--- exercise.js
+++ exercise.js
@@ -1,1 +1,17 @@
 console.log(githubData[6].payload.commits[0].message)
+
+
+
+for (let i = 0; i < githubData.length; i++) {
+    if (githubData[i].id == 8030403992) {
+        var author = githubData[i].payload.commits[0].author.name
+        var commitMsg = githubData[i].payload.commits[0].message
+        var date = githubData[i].created_at
+        var day = date.slice(0, 9)
+        var time = date.slice(11, 19)
+        break
+    }
+
+}
+
+console.log(`${author} created the commit message "${commitMsg}" at ${time} on ${day}`)
